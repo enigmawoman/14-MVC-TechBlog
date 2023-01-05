130ca4a10661f1f6bf0bfad9df137ac542fca548
enigmawoman <112570078+enigmawoman@users.noreply.github.com>
2023-01-05 18:13:13 +0000

    initial file structure and intital code basics added

---
+++ models/comment.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
+
+// Initialize Product model (table) by extending off Sequelize's Model class
+class Comment extends Model {}
+
+// set up fields and rules for Product model
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // user_name: {
+    //   type: DataTypes.STRING,
+    //   // prevents null values
+    //   allowNull: false,
+    // },
+    // date_posted:{
+    //     type: DataTypes.DATEONLY,
+    //     allowNull: false,
+    // },
+    user_comment: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'User',
+        key: 'id',
+      }
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Comment',
+  }
+);
+
+module.exports = Comment;
